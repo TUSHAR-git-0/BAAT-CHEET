1d52b659e1d96ea85d98cc2b6387dd8e80064b31
TUSHAR SHARMA <tusharsharma991193@gmail.com>
2026-07-19 00:54:19 +0530

    Remove frontend serving from backend

--- backend/server.js
+++ backend/server.js
@@ -33,13 +33,13 @@ app.use("/api/users", userRoutes);
 // });
 
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "frontend", "dist")));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "frontend", "dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
-  });
-}
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+//   });
+// }
 
 
 server.listen(PORT, () => {
